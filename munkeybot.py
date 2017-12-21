f07cd6a7beea20d3b025b25a622012b4a7201536
Huy Vu <huyvu9592@gmail.com>
2017-12-21 18:38:48 -0500

    a

--- munkeybot.py
+++ munkeybot.py
@@ -5,16 +5,18 @@ import pickle
 import os
 
 client = discord.Client()
+client.login("MzYyNzg0MTkwNTEwNTMwNTYw.DK3s5Q.OgAO_J3iPi7zvHa4IMvvFwD957g");
 
 @client.event
 async def on_ready():
 	print('Logged in as')
 	print(client.user.name)
 	print(client.user.id)
-	print('------')
+
 	
 @client.event
 async def on_message(message):
 	if message.content.startswith('!'):
 		await client.send_message(message.channel, 'Hello!')
 client.run('MzkzMjkwMjUwMzgzOTE3MDU3.DRznpA.-uud2vwbqHUvPlRI7DgjYt-kh4I')
+#client.run("MzYyNzg0MTkwNTEwNTMwNTYw.DK3s5Q.OgAO_J3iPi7zvHa4IMvvFwD957g");
